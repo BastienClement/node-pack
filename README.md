1e5a2e56d1dd9aa40497621353e7c2017b1443ad
galedric <g@ledric.me>
2014-06-21 15:29:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,184 @@
+node-pack
+=========
+
+```
+npm install node-pack
+```
+
+- [x] Compact binary data format
+- [x] Encode `null`, `boolean`, `number`, `string`, `Buffer`, `Array` and `Object`
+- [x] Perform string deduplication
+- [x] Handle circular references
+- [x] Can take advantage of a predefined list of common values
+
+## API
+
+`var Pack = require('node-pack');`
+
+### `Pack.encode(value, [dictionary])`
+
+Encode `value` and return its serialized `Buffer`.
+
+### `Pack.decode(buffer, [dictionary])`
+
+Decode `buffer` and return its deserialized value.
+
+### Dictionnary
+
+The dictionary is a optional list of commonly used values (up to 127 entries).
+
+When the encoder encounter a value from the dictionary, it can emit a single byte in the serialized form.
+
+The dictionary is not embedded in the serialized form. It's up to you to ensure that the decoder receive the dictionary used for encoding.
+
+```js
+var data = { hello: "world" };
+var dict = ["hello", "world"];
+
+var serialized = Pack.encode(data, dict);
+// <Buffer 07 01 80 81> (only 4 bytes)
+
+Pack.decode(serialized, dict);
+// { hello: "world" }
+```
+
+Values in the dictionary are tested with the identity operator (`===`). While it is not impossible for objects or arrays to be contained inside the dictionnary, it is mostly useful with scalar data like expected object keys.
+
+## Format
+
+### Endianness
+
+Multi-byte numbers are always big-endian.
+
+### Serialized value
+
+A serialized value is:
+
+```
+<TAG> <DATA>?
+```
+
+The tag describes the type of the value. Some tags are followed by additional data.
+
+### Tags
+
+A tag is a 8-bits unsigned integer with the following structure:
+
+```
+ MSB               LSB
++---+-------+---------+
+| 0 | 0 0 0 | 0 0 0 0 |
++---+-------+---------+
+  |     |        |
+  |     |        +-- BaseType   Tag & 0x0F
+  |     +-- Qualifier           Tag & 0x70 >> 4
+  +-- DictEntry                 Tag & 0x80 >> 7
+```
+
+If the `DictEntry` bit is `1`, this value is a dictionary reference and the 7 least significant bits are the dictionary index of this value. No optional data are present (it's a one byte value consisting only of its tag).
+
+Else, the tag is interpreted as follows:
+
+| BaseType | Name                  | Description                                |
+|----------|-----------------------|--------------------------------------------|
+| `1`      | Direct value          | Simple value derived from the qualifier    |
+| `2`      | Integer               | 8/16/32-bits signed or unsigned integes    |
+| `3`      | Float                 | IEEE float or double number                |
+| `4`      | String                | UTF-8 string                               |
+| `5`      | Buffer                | Raw binary buffers                         |
+| `6`      | List                  | List of values                             |
+| `7`      | Structure             | Key/Value object structure                 |
+| `8`      | String back reference | References to already serialized string    |
+| `9`      | List back reference   | References to already serialized list      |
+| `10`     | Struct back reference | References to already serialized structure |
+
+### Direct value
+
+A direct value is derived from its qualifier and require no additional data.
+
+| Qualifier | Value   |
+|-----------|---------|
+| `0`       | `null`  |
+| `1`       | `false` |
+| `2`       | `true`  |
+
+### Integer
+
+The 3-bits qualifier for integers is composed of 1 sign bit and 2 width bits.
+
+The integer is stored in the additional data for this value.
+
+| Qualifier | Sign     | Width   |
+|-----------|----------|---------|
+| `0`       | Unsigned | 8 bits  |
+| `1`       | Unsigned | 16 bits |
+| `2`       | Unsigned | 32 bits |
+| `3`       | Unsigned | 64 bits |
+| `4`       | Signed   | 8 bits  |
+| `5`       | Signed   | 16 bits |
+| `6`       | Signed   | 32 bits |
+| `7`       | Signed   | 64 bits |
+
+*Note: 64 bits integers are not available in this implementation*
+
+### Float / double
+
+The number is stored in the additional data for this value.
+
+| Qualifier | Type                |
+|-----------|---------------------|
+| `0`       | IEEE 32-bits float  |
+| `1`       | IEEE 64-bits double |
+
+*Note: this implementation always writes doubles*
+
+### Lengthy types
+
+Strings, Buffers, Arrays and Objects are called lengthy types because they are associated with a `length` property and use a similar qualifier structure.
+
+The qualifier indicate the width of the `length` property (an unsigned integer).
+
+| Qualifier | Width   |
+|-----------|---------|
+| `0`       | 8 bits  |
+| `1`       | 16 bits |
+| `2`       | 32 bits |
+| `3`       | 64 bits |
+
+Additional data for the value are prepended by this `length` integer in the form `<TAG> <LENGTH> <DATA>`.
+
+Back references are not stricly a lengthy type but use the same qualifier strucure, use the `length` as the back reference index and has no additional data after the `length` integer.
+
+*Note: 64 bits lengths are not available in this implementation*
+
+### String
+
+Strings are UTF-8 encoded. The `length` property is the byte-length of the string. Following data are the raw string bytes.
+
+### Buffer
+
+Buffers are raw binary data. The `length` property is the byte-length of the buffer. Following data are the raw buffer bytes.
+
+### Array
+
+Arrays are ordered lists of elements. The `length` property is the number of elements in the array.
+
+Following data are concatenated VCS-encoded elements representation.
+
+### Structure
+
+Structures is a key/value container of elements. Keys are strings, values can be of any type. The `length` property is the number of elements in the structure.
+
+Following data are concatenated serialized key representation and serialized value reprensentation, for every elements in sequence.
+
+### String/List/Structure back reference
+
+The `length` property is the back reference index. No additional data are required for back references.
+
+When the encoder process a string, an array or an object value, it stores it in a back references list. When the same value is processed again, a back reference to the previously encoded value is written. Since values are decoded in the same order, the back reference index is referencing the same previous value.
+
+This handle both string deduplication and circular references.
+
+## License
+
+MIT
---
+++ pack.js
@@ -0,0 +1,300 @@
+/*
+	Copyright (c) 2014 Bastien Clément
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var BufferReader = require("./reader");
+var BufferWriter = require("./writer");
+
+/*
+ * Encode
+ */
+
+function lengthy_type(writer, type, length) {
+	var bit_len, qualifier;
+
+	if (length < 0x100) {
+		writer.writeUInt8(type);
+		writer.writeUInt8(length);
+	} else if (length < 0x10000) {
+		writer.writeUInt8(type | 0x10);
+		writer.writeUInt16(length);
+	} else if (length < 0x100000000) {
+		writer.writeUInt8(type | 0x20);
+		writer.writeUInt32(length);
+	} else {
+		throw new Error("Unable to encode length: " + length);
+	}
+}
+
+function PackEncode(value, dictionary, backrefs, writer) {
+	if (!backrefs) backrefs = { strings: [], lists: [], structs: [] };
+
+	if (!writer) {
+		writer = new BufferWriter();
+		PackEncode(value, dictionary, backrefs, writer);
+		return writer.getBuffer();
+	}
+
+	var type = typeof value;
+
+	if (type === "object") {
+		if (!value) {
+			type = "null";
+		} else if (Array.isArray(value)) {
+			type = "array";
+		} else if (Buffer.isBuffer(value)) {
+			type = "buffer";
+		}
+	} else if (dictionary) {
+		var idx = dictionary.indexOf(value);
+		if (idx > -1 && idx < 128) {
+			writer.writeUInt8(idx + 128);
+			return;
+		}
+	}
+
+	if (type === "string" || type === "object" || type === "array") {
+		var backrefs_set, backref_type;
+		switch (type) {
+			case "string": backrefs_set = backrefs.strings; backref_type = 0x08; break;
+			case "array":  backrefs_set = backrefs.lists;   backref_type = 0x09; break;
+			case "object": backrefs_set = backrefs.structs; backref_type = 0x0A; break;
+		}
+
+		var backref = backrefs_set.indexOf(value);
+		if (backref > -1) {
+			lengthy_type(writer, backref_type, backref);
+			return;
+		}
+	}
+
+	switch (type) {
+		case "undefined":
+		case "null":
+			writer.writeUInt8(0x01);
+			return;
+
+		case "boolean":
+			writer.writeUInt8(value ? 0x21 : 0x11);
+			return;
+
+		case "number":
+			// Integer (in 32 bits range)
+			if ((value % 1) === 0 && value >= -2147483648 && value <= 4294967295) {
+				if (value < 0) {
+					// Signed
+					if (value >= -128) {
+						writer.writeUInt8(0x42);
+						writer.writeInt8(value);
+					} else if (value >= -32768) {
+						writer.writeUInt8(0x52);
+						writer.writeInt16(value);
+					} else {
+						writer.writeUInt8(0x62);
+						writer.writeInt32(value);
+					}
+				} else {
+					// Unsigned
+					if (value < 256) {
+						writer.writeUInt8(0x02);
+						writer.writeUInt8(value);
+					} else if (value < 65536) {
+						writer.writeUInt8(0x12);
+						writer.writeUInt16(value);
+					} else {
+						writer.writeUInt8(0x22);
+						writer.writeUInt32(value);
+					}
+				}
+
+				return;
+			}
+
+			// Double
+			writer.writeUInt8(0x13);
+			writer.writeDouble(value);
+			return;
+
+		case "string":
+			backrefs.strings.push(value);
+			lengthy_type(writer, 0x04, Buffer.byteLength(value, "utf8"));
+			writer.writeString(value);
+			return;
+
+		case "buffer":
+			lengthy_type(writer, 0x05, value.length);
+			writer.writeBuffer(value);
+			return;
+
+		case "array":
+			backrefs.lists.push(value);
+
+			var length = value.length;
+			lengthy_type(writer, 0x06, length);
+
+			for(var i = 0; i < length; ++i) {
+				PackEncode(value[i], dictionary, backrefs, writer);
+			}
+
+			return;
+
+		case "object":
+			backrefs.structs.push(value);
+
+			var keys = Object.keys(value);
+			var length = keys.length;
+			lengthy_type(writer, 0x07, length);
+
+			for(var i = 0; i < length; ++i) {
+				var key = keys[i];
+				PackEncode("" + key, dictionary, backrefs, writer)
+				PackEncode(value[key], dictionary, backrefs, writer);
+			}
+
+			return;
+
+		default:
+			throw new Error("Unable to encode type: " + type);
+	}
+}
+
+/*
+ * Decode
+ */
+
+function PackDecode(buffer, dictionary, backrefs) {
+	if (!(buffer instanceof BufferReader)) buffer = new BufferReader(buffer);
+	if (!backrefs) backrefs = { strings: [], lists: [], structs: [] };
+
+	var type = buffer.nextUInt8();
+
+	// Dictionary value
+	if (type & 0x80) {
+		var idx = type & 0x7F;
+		if (dictionary && idx < dictionary.length) {
+			return dictionary[idx];
+		}
+
+		throw new Error("Undefined dictionary value: " + idx);
+	}
+
+	var base_type = type & 0x0F;
+	var qualifiers = (type & 0x70) >> 4;
+
+	switch (base_type) {
+		case 1: // Simple
+			switch (qualifiers) {
+				case 0: return null;
+				case 1: return false;
+				case 2: return true;
+				default:
+					throw new Error("Unknown simple qualifier: " + qualifiers);
+			}
+
+		case 2: // Integer
+			switch (qualifiers) {
+				case 0: return buffer.nextUInt8();
+				case 1: return buffer.nextUInt16();
+				case 2: return buffer.nextUInt32();
+				case 4: return buffer.nextInt8();
+				case 5: return buffer.nextInt16();
+				case 6: return buffer.nextInt32();
+				default:
+					console.log(buffer.offset);
+					throw new Error("Unknown integer qualifiers: " + qualifiers);
+			}
+
+		case 3: // Float
+			switch (qualifiers) {
+				case 0: return buffer.nextFloat();
+				case 1: return buffer.nextDouble();
+				default:
+					throw new Error("Unknown float qualifier: " + qualifiers);
+			}
+
+		case 4: // String
+		case 5: // Buffer
+		case 6: // List
+		case 7: // Struct
+		case 8: // String backref
+		case 9: // List backref
+		case 10: // Struct backref
+			var length;
+			switch (qualifiers) {
+				case 0: length = buffer.nextUInt8(); break;
+				case 1: length = buffer.nextUInt16(); break;
+				case 2: length = buffer.nextUInt32(); break;
+				default:
+					throw new Error("Unknown length qualifier: " + qualifiers);
+			}
+
+			switch (base_type) {
+				case 4: // String
+					var string = buffer.nextString(length);
+					backrefs.strings.push(string);
+					return string;
+
+				case 5: // Buffer
+					return buffer.nextBuffer(length);
+
+				case 6: // List
+					var list = [];
+					backrefs.lists.push(list);
+					for (var i = 0; i < length; ++i) {
+						list.push(PackDecode(buffer, dictionary, backrefs));
+					}
+					return list;
+
+				case 7: // Struct
+					var struct = {};
+					backrefs.structs.push(struct);
+					for (var i = 0; i < length; ++i) {
+						var key = PackDecode(buffer, dictionary, backrefs);
+						if (typeof key !== "string") {
+							throw new Error("Invalid struct key: " + key);
+						}
+						struct[key] = PackDecode(buffer, dictionary, backrefs);
+					}
+					return struct;
+
+				case 8: // String backref
+					if (length < backrefs.strings.length) return backrefs.strings[length];
+					throw new Error("Undefined string back reference: " + length);
+
+				case 9: // List backref
+					if (length < backrefs.lists.length) return backrefs.lists[length];
+					throw new Error("Undefined list back reference: " + length);
+
+				case 10: // Struct backref
+					if (length < backrefs.structs.length) return backrefs.structs[length];
+					throw new Error("Undefined structure back reference: " + length);
+			}
+
+		default:
+			throw new Error("Unknown base type: " + base_type);
+	}
+}
+
+module.exports = {
+	encode: PackEncode,
+	decode: PackDecode
+};
